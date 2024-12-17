537bbef6b10f843e3d79fcd7861ccd45e3fc674c
Степан Дрогин <sedrogin@basistech.ru>
2024-12-17 12:19:48 +0300

    Добавил докер

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Lottie from "react-lottie";
 import animationData from "../../assets/animations/chefbot.json";
-import styles from "./SplashScreen.module.css";
+import styles from './SplashScreen.module.css'
 
 const SplashScreen = ({ onFinish }) => {
   const [progress, setProgress] = useState(0);
