c68da734b020afda1709f59d681f16ad8390b560
kz <lakkq@mail.ru>
2024-12-21 18:25:25 +0300

    вернул экран загрузки (куда он пропал?)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Title from './pages/Title/Title'
 import Form from './pages/Form/Form'
 import About from './pages/About/About'
 import RecipePage from './pages/RecipePage/RecipePage'
+import SplashScreen from "./components/SplashScreen/SplashScreen";
 
 const App = () => {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return <SplashScreen />;
+  }
+
   return (
     <Router>
       <Routes>
